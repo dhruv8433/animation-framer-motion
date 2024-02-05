4e7a14714be06c00e2d96629421f6ea634c5d05a
dhruv8433 <dhruvrsoni04@gmail.com>
2024-02-05 09:37:41 +0530

    init 🚀

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { motion } from "framer-motion";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* -----------DIV------------ */}
+
+      {/* coming div from upper side */}
+      <motion.div
+        initial={{ opacity: 0, y: -100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
+        <h1>Welcome to My Framer Motion App</h1>
+      </motion.div>
+
+      {/* coming div from lower side */}
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
+        <h1>Welcome to My Framer Motion App</h1>
+      </motion.div>
+
+      {/* coming div from right side */}
+      <motion.div
+        initial={{ opacity: 0, x: 100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+      >
+        <h1>Welcome to My Framer Motion App</h1>
+      </motion.div>
+
+      {/* coming div from left side */}
+      <motion.div
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+      >
+        <h1>Welcome to My Framer Motion App</h1>
+      </motion.div>
+
+      {/* while tap it jump like left to right */}
+      <motion.div whileFTap={{ scale: 0.8 }}>Tap</motion.div>
+
+      {/* -----------BUTTON------------ */}
+
+      {/* on click bounch effect and hover transition */}
+      <motion.button
+        whileHover={{
+          scale: 1.2,
+          transition: { duration: 1 },
+        }}
+        whileTap={{ scale: 0.9 }}
+        title="click"
+      >
+        Hello
+      </motion.button>
+
+      {/* -----------LINK------------ */}
+
+      <motion.a
+        whileHover={{ scale: 1.2 }}
+        onHoverStart={(e) => {}}
+        onHoverEnd={(e) => {}}
+      />
+
+      {/* -----------INPUT------------ */}
+
+      <motion.input whileFocus={{ scale: 1.2 }} />
+
+      
     </div>
   );
-}
+};
 
 export default App;
